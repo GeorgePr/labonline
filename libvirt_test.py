7bb597b9986d15b95f6f85336bc679e5cb9314e9
Georgios Proimakis <gproim@gmail.com>
2020-11-12 14:01:03 +0200

    Added domain creation via XML, simple script to create + destroy a domain

--- libvirt_test.py
+++ libvirt_test.py
@@ -1,6 +1,43 @@
 import libvirt
+import sys
+
+# Initialize connection
 
 conn = libvirt.open("qemu:///system")
+
+# Create and start domain based on sample_domain.xml
+
+xml_file = 'sample_domain.xml'
+xml_open = open(xml_file)
+xmlconfig = xml_open.read()
+dom = conn.defineXML(xmlconfig)
+
+"""
+if dom == None:
+	print('Failed to define a domain from XML definition', file = sys.sdterr)
+	exit(1)
+
+if dom.create() < 0:
+	print('Cannot boot domain', file = sys.stderr)
+"""
+
+# List domains
+
 names = conn.listDefinedDomains()
 print(names)
 
+# Start domain
+
+dom.create()
+print('Guest ' + dom.name() + ' has booted', file = sys.stderr)
+
+# Wait for keypress to remove domain
+
+input('Press Enter to remove domain...')
+dom.destroy()
+dom.undefine()
+
+# Close connection
+
+conn.close()
+exit(0)
